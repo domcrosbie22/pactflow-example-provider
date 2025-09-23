72630f35f0098cb2fb4cc520ba9db88fd525bc1b
Dom Crosbie <domcrosbie22@gmail.com>
2025-09-23 14:40:39 +0100

    Break Dev

--- src/product/pact.setup.js
+++ src/product/pact.setup.js
@@ -21,23 +21,34 @@ const setupServer = () => {
 };
 
 const stateHandlers = {
-  "products exists": () => {
-    controller.repository.products = new Map([
-      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
-    ]);
-  },
   "products exist": () => {
     controller.repository.products = new Map([
-      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
+      ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
+      ["10", new Product("10", "28 Degrees", 0.2)],
+      ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
+    ]);
+  },
+  "a product with ID 09 exists": () => {
+    controller.repository.products = new Map([
+      ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
     ]);
   },
   "a product with ID 10 exists": () => {
     controller.repository.products = new Map([
-      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
+      ["10", new Product("10", "28 Degrees", 0.2)],
     ]);
   },
-  "a product with ID 11 does not exist": () => {
-    controller.repository.products = new Map();
+  "a product with ID 11 exists": () => {
+    controller.repository.products = new Map([
+      ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
+    ]);
+  },
+  "a product with ID 12 does not exist": () => {
+    controller.repository.products = new Map([
+      ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
+      ["10", new Product("10", "28 Degrees", 0.2)],
+      ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
+    ]);
   },
 };
 
